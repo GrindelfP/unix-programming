d91550d01f4e6fabb8a0cd6c72b45b7f47233d75
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-03-06 12:47:00 +0300

    complete script6

--- task2/script6.sh
+++ task2/script6.sh
@@ -3,8 +3,8 @@ path=$2
 
 if [[ $# -ge 2 ]]; then
   
-  if [[ -d $path ]]; then
-    echo "\033[0;31mERROR! Illegal number of arguments provided!\033[0m"
+  if [[ ! -d $path ]]; then
+    echo "\033[0;31mERROR! Provided path is not a directory!\033[0m"
   else
 
     if [[ $flag == "-f" ]]; then
