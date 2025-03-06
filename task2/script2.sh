88bf20973e54a3af8bcaa48926ed888bf7d5c905
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-03-06 10:33:41 +0300

    task with shell script

---
+++ task2/script2.sh
@@ -0,0 +1,13 @@
+echo "hello there!"
+
+date=`date`
+
+echo "Today is -" $date
+
+a=5
+b=10
+
+echo "a + b"
+echo `expr $a + $b`
+echo `expr $a \* $b`
+
---
+++ task2/script3.sh
@@ -0,0 +1,10 @@
+echo "Script name is -" $0
+
+echo "Process ID is -" $$
+
+echo "Number of params is -" $#
+
+echo "Params as single line:" $*
+
+echo "Params as discrete lines:" $@
+
