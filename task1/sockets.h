49ee5094d8c6f4acc9655c69323a364e5c9bdc1c
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-03-12 13:08:14 +0300

    began task 2

---
+++ task1/sockets.h
@@ -0,0 +1,13 @@
+/*
+ Common header file for socket operations.
+ Created by GrindelfP on 2025-03-12.
+*/
+
+#ifndef sockets_h
+#define sockets_h
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#endif
+
